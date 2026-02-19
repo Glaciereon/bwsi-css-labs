75bea02d2a4e864496da9e1ed26791e2fb8cbf06
Gleb Kozlov <glebmkozlov@gmail.com>
2026-02-19 16:16:43 -0500

    Update lab_1a.py
    Added my description and added "\n" to format it nicely

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Gleb Kozlov" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hello everyone! My name is Gleb Kozlov, I'm from Massachusetts, and I am a junior.\nI really enjoy all sorts of CAD and 3D printing and enjoy making things for a variety of different purposes,\nbut my favorite is definitely model rocketry parts for the ARC competition. I also love jazz music\nand play in a small school ensemble with a few friends (I play the upright bass!).")
 if __name__ == "__main__":
     main()
